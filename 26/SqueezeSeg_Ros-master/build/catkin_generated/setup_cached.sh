bffc2f2871fbf6ca55e221e10812dc791a080083
yangwuw <986223816@qq.com>
2024-06-20 18:06:27 +0800

    26

---
+++ 26/SqueezeSeg_Ros-master/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/mc/autodriver/26/SqueezeSeg_Ros-master/devel:$CMAKE_PREFIX_PATH"
+export PWD='/home/mc/autodriver/26/SqueezeSeg_Ros-master/build'
+export ROSLISP_PACKAGE_DIRECTORIES='/home/mc/autodriver/26/SqueezeSeg_Ros-master/devel/share/common-lisp'
+export ROS_PACKAGE_PATH='/home/mc/autodriver/26/SqueezeSeg_Ros-master/src:/opt/ros/noetic/share'
